697b98ac92c5e0a980890d51dc029caa72e53995
shiyuan598 <wangyuanhpu@163.com>
2019-09-22 14:13:21 +0800

    modify the save file name

--- EncodeGeoJSON/index.js
+++ EncodeGeoJSON/index.js
@@ -3,7 +3,7 @@ let turf = require('@turf/turf')
 let fs = require('fs')
 let geobuf = require('geobuf')
 let Pbf = require('pbf')
-function encodeData (url) {
+function encodeData (url, outGeoJSON, outGeoBuf) {
     axios.get(url).then((function (res) {
         let arr = (res.data.data)
         let features = []
@@ -12,21 +12,21 @@ function encodeData (url) {
             features.push(feature)
         })
         let featureCollection = turf.featureCollection(features)
-        fs.writeFile('./data/features.json', JSON.stringify(featureCollection), function(error){
+        fs.writeFile(outGeoJSON, JSON.stringify(featureCollection), function(error){
             if(error){
-                console.info('error')
+                console.info('geojson error')
             }else {
-                console.info('ok')
+                console.info('geojson ok')
             }
         })
 
         let buffer = geobuf.encode(featureCollection, new Pbf())
         console.info(buffer)
-        fs.writeFile('./data/geobuf.pbf', buffer, function(error){
+        fs.writeFile(outGeoBuf, buffer, function(error){
             if(error){
-                console.info('error')
+                console.info('geobuf error')
             }else {
-                console.info('ok')
+                console.info('geobuf ok')
             }
         })
         console.info()
@@ -35,17 +35,18 @@ function encodeData (url) {
 
 function decodeData(url) {
     axios.get(url, {
-        responseType: 'arraybuffer'
+        responseType: 'arraybuffer' // note: responseType must be 'arraybuffer'
     }).then((function (res) {
         let data = res.data
         let geojson = geobuf.decode(new Pbf(data))
-        // console.info(geojson)
         console.info(JSON.stringify(geojson))
     }))
 }
 
 let url = 'http://localhost:9004/data/lamp.min.json'
-encodeData(url)
+let  outGeoJSON = './data/lamp.geojson.json'
+let  outGeoBuf = './data/lamp.geobuf.bpf'
+encodeData(url, outGeoJSON, outGeoBuf)
 
-url = 'http://localhost:9004/data/geobuf.pbf'
+url = 'http://localhost:9004/data/lamp.geobuf.bpf'
 decodeData(url)
