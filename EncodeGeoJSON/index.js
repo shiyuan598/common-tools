6cedd9c70c4985c6028d9662078fbfa07d90ec20
wangshiyuan <wangshiyuan@navinfo.com>
2019-09-23 13:52:47 +0800

    reduce data

--- EncodeGeoJSON/index.js
+++ EncodeGeoJSON/index.js
@@ -3,12 +3,39 @@ let turf = require('@turf/turf')
 let fs = require('fs')
 let geobuf = require('geobuf')
 let Pbf = require('pbf')
+
+function reduceData(url, outJSON) {
+    axios.get(url).then((function (res) {
+        let arr = (res.data.data)
+        let data = []
+        let scale = 1000000
+        let xBase = 103800000
+        let yBase = 1300000
+        arr.forEach(item => {
+            data.push([item[0].toFixed(6) * scale - xBase, item[1].toFixed(6) * scale - yBase, item[2]])
+        })
+        let reduceData = {
+            xBase: xBase,
+            yBase: yBase,
+            scale: scale,
+            data: data
+        }
+        fs.writeFile(outJSON, JSON.stringify(reduceData), function (error) {
+            if (error) {
+                console.info('reduceData error')
+            } else {
+                console.info('reduceData ok')
+            }
+        })
+    }))
+}
+
 function encodeData (url, outGeoJSON, outGeoBuf) {
     axios.get(url).then((function (res) {
         let arr = (res.data.data)
         let features = []
         arr.forEach(item => {
-            let feature = turf.point([item[0], item[1]], {'angle': item[2]})
+            let feature = turf.point([Number(item[0].toFixed(6)), Number(item[1].toFixed(6))], {'angle': item[2]})
             features.push(feature)
         })
         let featureCollection = turf.featureCollection(features)
@@ -42,10 +69,13 @@ function decodeData(url) {
     }))
 }
 
-let url = 'http://localhost:9004/data/lamp.min.json'
-let  outGeoJSON = './data/lamp.geojson.json'
-let  outGeoBuf = './data/lamp.geobuf.bpf'
-encodeData(url, outGeoJSON, outGeoBuf)
+let url = 'http://localhost:9001/data/sign.min.json'
+let  outGeoJSON = './data/lamp.geojson.min.json'
+let  outGeoBuf = './data/lamp.geobuf.min.bpf'
+// encodeData(url, outGeoJSON, outGeoBuf)
 
-url = 'http://localhost:9004/data/lamp.geobuf.bpf'
-decodeData(url)
+let outJSON = './data/sign.reduce.min.json'
+reduceData(url, outJSON)
+
+url = 'http://localhost:9001/data/lamp.geobuf.bpf'
+// decodeData(url)
